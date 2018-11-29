88311f0bbef14c7ac0652e64c911bca30a4858ed
Pawan Tejwani <ptptejwani@gmail.com>
2018-11-29 18:28:48 +0530

    checking two way bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
+
+// this is the default component
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App!';
+  // title = 'My First Angular App!';
+  name = 'Pawan Tejwani';
 }
